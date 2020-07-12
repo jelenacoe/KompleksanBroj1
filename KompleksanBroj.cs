7975943b937a8d9e40aab78caf64b09fac213b5b
jelenacoe <58305766+jelenacoe@users.noreply.github.com>
2020-07-12 11:45:56 +0200

    First commit

---
+++ KompleksanBroj.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KompleksanBroj
+{
+    class KompleksanBroj
+    {
+        private int realanDeo;
+        private int imaginaranDeo;
+        private static int brojInstanci = 0;
+
+        public KompleksanBroj(int realan, int imaginaran)
+        {
+            this.realanDeo = realan;
+            this.imaginaranDeo = imaginaran;
+            ++BrojInstanci;
+        }
+
+
+        public int RealanDeo
+        {
+            get { return realanDeo; }
+        }
+
+
+        public int ImaginaranDeo
+        {
+            get { return imaginaranDeo; }
+        }
+
+
+        public double ApsolutnaVrednost
+        {
+            get 
+            {
+                double kvadrat = (realanDeo * realanDeo) + (imaginaranDeo * imaginaranDeo);
+                return Math.Sqrt(kvadrat);
+            }
+        }
+
+
+        public override string ToString()
+        {
+            string im = ImaginaranDeo < 0 ? " - " + (-ImaginaranDeo) : " + " + ImaginaranDeo;
+            return RealanDeo + im + "i";
+        }
+
+
+        public static int BrojInstanci
+        {
+            get { return brojInstanci; }
+
+            protected set { brojInstanci = value; }
+        }
+
+        // preklapanje operatora +
+        public static KompleksanBroj operator +(KompleksanBroj k1, KompleksanBroj k2)
+        {
+            return new KompleksanBroj(k1.RealanDeo + k2.RealanDeo, k1.ImaginaranDeo + k2.ImaginaranDeo);
+        }
+
+        // preklapanje operatora -
+        public static KompleksanBroj operator -(KompleksanBroj k1, KompleksanBroj k2)
+        {
+            return new KompleksanBroj(k1.RealanDeo - k2.RealanDeo, k1.ImaginaranDeo - k2.ImaginaranDeo);
+        }
+
+        // metoda za sabiranje
+        public static KompleksanBroj Saberi(KompleksanBroj k1, KompleksanBroj k2)
+        {
+            return new KompleksanBroj(k1.RealanDeo + k2.RealanDeo, k1.ImaginaranDeo + k2.ImaginaranDeo);
+        }
+
+        // metoda za oduzimanje
+        public static KompleksanBroj Oduzmi(KompleksanBroj k1, KompleksanBroj k2)
+        {
+            return new KompleksanBroj(k1.RealanDeo - k2.RealanDeo, k1.ImaginaranDeo - k2.ImaginaranDeo);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KompleksanBroj
+{
+    class Program
+    {
+        static void Main()
+        {
+            KompleksanBroj k1 = new KompleksanBroj(1, -2);
+            KompleksanBroj k2 = new KompleksanBroj(3, -4);
+
+            KompleksanBroj k3 = k1 - k2;
+            KompleksanBroj k4 = k1 + k2;
+
+            KompleksanBroj k5 = KompleksanBroj.Oduzmi(k1, k2);
+            KompleksanBroj k6 = KompleksanBroj.Saberi(k1, k2);
+
+            Console.WriteLine("k1 = {0}, apsolutna vrednost broja je {1}", k1, k1.ApsolutnaVrednost);
+            Console.WriteLine("k2 = {0}, apsolutna vrednost broja je {1}", k2, k2.ApsolutnaVrednost);
+
+            Console.WriteLine("Preklapanje operatora : k1 - k2 = {0}, apsolutna vrednost broja je {1}", k3, k3.ApsolutnaVrednost);
+            Console.WriteLine("Pozivanje metode : k1 - k2 = {0}, apsolutna vrednost broja je {1}", k5, k3.ApsolutnaVrednost);
+
+            Console.WriteLine("Preklapanje operatora : k1 + k2 = {0}, apsolutna vrednost broja je {1}", k4, k4.ApsolutnaVrednost);
+            Console.WriteLine("Pozivanje metode :  k1 + k2 = {0}, apsolutna vrednost broja je {1}", k6, k4.ApsolutnaVrednost);
+
+            Console.WriteLine("Ukupan broj kreiranih brojeva je {0}", KompleksanBroj.BrojInstanci);
+
+            Console.ReadKey();
+        }
+    }
+}
